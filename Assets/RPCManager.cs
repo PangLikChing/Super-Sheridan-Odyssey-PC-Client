81c3398f028a19d187f434f0d5fe300259f876aa
Qiao <qiaokuanghua@gmail.com>
2022-04-22 02:06:56 -0400

    Mobile Win Screen finally shows up by using photon event

--- Assets/RPCManager.cs
+++ Assets/RPCManager.cs
@@ -1,16 +1,17 @@
+using ExitGames.Client.Photon;
 using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class RPCManager : MonoBehaviour
 {
-    PhotonView photonView;
-
+    public UnityEvent gameWon;
+    public UnityEvent gameLost;
+    public const byte EventCode = 0;
     private void Start()
     {
-        photonView = GetComponent<PhotonView>();
-
         LevelManager.Instance.Victory.AddListener(GameWon);
 
         LevelManager.Instance.GameOver.AddListener(GameLost);
@@ -18,11 +19,13 @@ public class RPCManager : MonoBehaviour
 
     public void GameWon()
     {
-        photonView.RPC("OnVictory", RpcTarget.Others);
+        //photonView.RPC("OnVictory", RpcTarget.Others);
+        PhotonNetwork.RaiseEvent(EventCode, 0, null, SendOptions.SendReliable);
     }
 
     public void GameLost()
     {
-        photonView.RPC("OnLose", RpcTarget.Others);
+        //photonView.RPC("OnLose", RpcTarget.Others);
+        PhotonNetwork.RaiseEvent(EventCode, 1, null, SendOptions.SendReliable);
     }
 }
